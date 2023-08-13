8a91799c575f861e5d009f89684c65381042c194
bakakoozin <cyrilpebre@gmail.com>
2023-08-13 19:53:09 +0200

    une tentative qui ne fonctionne pas

--- index.js
+++ index.js
@@ -5,17 +5,49 @@ const app = express()
 const { MongoClient } = require('mongodb')
 
 const client = new MongoClient(MONGO_URI)
+const njord = client.db('njord_prod')
+const valhalla = client.db('valhalla_dev')
 
 app.get('/', async (_req, res) => {
-    await client.connect()
+  await client.connect()
 
-    const database = client.db('njord_prod')
-    const Users = database.collection('users')
-    const users= await Users.find({}).toArray()
-    
-    res.json(users)
-})
+    const usersACollection = valhalla.collection('users')
+    const usersBCollection = njord.collection('users')
+        
+          // Récupération des deux listes d'utilisateurs
+          const usersA = await usersACollection.find({}).toArray();
+          const usersB = await usersBCollection.find({}).toArray();
+      
+          // Création d'un objet pour stocker les utilisateurs par e-mail avec leurs wallet
+          const usersByMail = {};
+      
+          // Remplissage de l'objet utilisateursParEmail à partir de la liste utilisateursB
+          usersB.forEach(usersB => {
+            const email = usersB.email;
+            const wallet = usersB.wallet;
+      
+            if (!usersByMail[email]) {
+              usersByMail[email] = { wallet: 0 };
+            }
+      
+            usersByMail[email].wallet += wallet;
+          });
+      
+          // Boucle sur la liste utilisateursA pour chercher et additionner les wallets
+          usersA.forEach(usersA => {
+            const email = usersA.email;
+      
+            if (usersByMail[email]) {
+              const walletToAdd = usersByMail[email].wallet;
+              usersA.wallet += walletToAdd;
+            }
+          });
+      
+          // Affichage des utilisateurs de la liste utilisateursA avec le wallet additionné
+          usersA.forEach(usersA => {
+            console.log(usersA);
+        })
 
-app.listen(PORT, () => {
-    console.log(`Affichage sur le port ${PORT}`)
+res.json(usersB)
+
 })
